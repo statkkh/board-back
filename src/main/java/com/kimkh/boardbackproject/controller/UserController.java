8696203dc49c020b8d98c93cd4fa0822aeb997c9
khkim <stakkh4@gmail.com>
2023-10-10 16:24:34 +0900

    PatchNicknameResponseDto / patchNickname

--- src/main/java/com/kimkh/boardbackproject/controller/UserController.java
+++ src/main/java/com/kimkh/boardbackproject/controller/UserController.java
@@ -1,14 +1,21 @@
 package com.kimkh.boardbackproject.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import com.kimkh.boardbackproject.dto.request.user.PatchNicknameRequestDto;
 import com.kimkh.boardbackproject.dto.response.user.GetSignInUserResponseDto;
 import com.kimkh.boardbackproject.dto.response.user.GetUserResponseDto;
+import com.kimkh.boardbackproject.dto.response.user.PatchNicknameResponseDto;
 import com.kimkh.boardbackproject.service.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -33,6 +40,14 @@ public class UserController {
         @PathVariable("email") String email
     ) {
         ResponseEntity<? super GetUserResponseDto> response = userService.getUser(email);
+        return response;
+    }
+    @PatchMapping("/nickname")
+    public ResponseEntity<? super PatchNicknameResponseDto> patchNickname(
+        @RequestBody @Valid PatchNicknameRequestDto requestBody,
+        @AuthenticationPrincipal String email
+    ){
+        ResponseEntity<? super PatchNicknameResponseDto> response = userService.patchNickname(requestBody, email);
         return response;
     }
 
