3cefae0244eab263dd2669abe4aff0f5fcce78b7
khkim <stakkh4@gmail.com>
2023-10-11 11:06:42 +0900

    findTop3ByWriteDatetimeGreaterThanOrderByFavoriteCountDesc

--- src/main/java/com/kimkh/boardbackproject/repository/BoardViewRepository.java
+++ src/main/java/com/kimkh/boardbackproject/repository/BoardViewRepository.java
@@ -13,4 +13,5 @@ public interface BoardViewRepository extends JpaRepository<BoardViewEntity, Inte
 
     List<BoardViewEntity> findByOrderByWriteDatetimeDesc();
     List<BoardViewEntity> findByWriterEmailOrderByWriteDatetimeDesc(String email);
+    List<BoardViewEntity> findTop3ByWriteDatetimeGreaterThanOrderByFavoriteCountDesc(String writeDatetime);
 }
