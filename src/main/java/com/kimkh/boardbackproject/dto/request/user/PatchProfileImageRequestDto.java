ef2493899d3938e33b143d3606dba19027b1e0df
khkim <stakkh4@gmail.com>
2023-10-11 09:34:56 +0900

    PatchProfileImageRequestDto

---
+++ src/main/java/com/kimkh/boardbackproject/dto/request/user/PatchProfileImageRequestDto.java
@@ -0,0 +1,14 @@
+package com.kimkh.boardbackproject.dto.request.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PatchProfileImageRequestDto {
+    
+    
+    private String profileImage;
+}
